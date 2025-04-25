cd10d70259d1f62c18d6b8cf2879573e2f83efef
unknown12133 <mr.unknown.boy.022@gmail.com>
2025-04-25 11:19:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+ //$con=new mysqli('sql307.byetcluster.com','ezyro_38673444','tqwdc34y','ezyro_38673444_miilkin');
+ //$con=new mysqli('sql12.freesqldatabase.com','sql12774509','xrZCSQnnLj','sql12774509');
+$a='';
+$b='';
+if (isset($_POST['submit'])){
+    $a = $_POST['a'];
+    $b=$_POST['b'];       
+}
+// echo $a;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="a"><br>
+        <input type="text" name="b">
+        <button name="submit">submit</button>
+    </form>
+    <?php
+       echo $sum=$a+$b; ?>
+        
+</body>
+</html>
